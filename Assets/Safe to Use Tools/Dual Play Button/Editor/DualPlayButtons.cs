a8608fd68a6a39d875447a05d7b782a80be8b090
D3vCrow <chr.papachristoforou@gmail.com>
2025-11-23 12:05:29 +0200

    Animation Pose & Play Button

---
+++ Assets/Safe to Use Tools/Dual Play Button/Editor/DualPlayButtons.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.Overlays;
+using UnityEngine.UIElements;
+
+[Overlay(typeof(SceneView), "Dual Play Buttons")]
+public class PlayModeOverlay : Overlay
+{
+    public override VisualElement CreatePanelContent()
+    {
+        var root = new VisualElement();
+        root.style.flexDirection = FlexDirection.Row;
+
+        var full = new Button(() =>
+        {
+            EditorSettings.enterPlayModeOptionsEnabled = false;
+            EditorApplication.isPlaying = !EditorApplication.isPlaying;
+        })
+        { text = "▶ Reload" };
+
+        var fast = new Button(() =>
+        {
+            EditorSettings.enterPlayModeOptionsEnabled = true;
+            EditorSettings.enterPlayModeOptions =
+                EnterPlayModeOptions.DisableDomainReload |
+                EnterPlayModeOptions.DisableSceneReload;
+
+            EditorApplication.isPlaying = !EditorApplication.isPlaying;
+        })
+        { text = "⚡ Fast" };
+
+        root.Add(full);
+        root.Add(fast);
+        return root;
+    }
+}
